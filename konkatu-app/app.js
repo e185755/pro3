ba39eac873bb2cb22af1b54d4324af63681ccda6
DakujakuReiya <e185709@ie.u-ryukyu.ac.jp>
2020-07-27 20:46:40 +0900

    heroku up sippai

--- konkatu-app/app.js
+++ konkatu-app/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+/* 追加 ポートの適応 */
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', (req, res) => {
     res.render('login.ejs');
 });
